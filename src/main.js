f47d1e8460081066c46f5a583b47ac7219c9157b
Feng <516725062@qq.com>
2019-06-13 14:38:27 +0800

    更新初始化配置

--- src/main.js
+++ src/main.js
@@ -7,25 +7,21 @@ import router from './router'
 import 'lib-flexible'
 
 import store from './vuex/'
+
 Vue.use(store)
 
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI)
-
-import { Spinner } from 'mint-ui'
-import 'mint-ui/lib/style.css'
 Vue.component(Spinner.name, Spinner);
 
 Vue.config.productionTip = false
 
-import { log } from '@/components/common/utils'
+import {log} from '@/components/common/utils'
+
 Vue.prototype.log = log
 /* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    router,
-    store,
-    components: { App },
-    template: '<App/>'
+	el: '#app',
+	router,
+	store,
+	components: {App},
+	template: '<App/>'
 })
